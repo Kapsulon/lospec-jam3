c1965047d63896120b6fb18b36221617098a2f76
Kayzen6446 <ethan.eudes64@gmail.com>
2024-08-03 04:24:29 +0200

    added models + world + environnement blocking + shader + FISH

--- src/main.gd
+++ src/main.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 func _ready() -> void:
-    # Game.Start()
-    # Awesomeness.Add(9999)
-    # Jam.Complete()
-    pass
+	# Game.Start()
+	# Awesomeness.Add(9999)
+	# Jam.Complete()
+	pass
